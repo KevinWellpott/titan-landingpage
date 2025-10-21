90678a951ef25f1b5fc6d3a3175682f48b599099
KevinWellpott <rasmus.paweletz@gmail.com>
2025-10-21 22:53:51 +0200

    Navbar v.1

--- script.js
+++ script.js
@@ -21,4 +21,9 @@ if (kevinButton) {
   });
 }
 
+const burgerBtn = document.getElementById("burger-btn");
+const navMenu = document.getElementById("nav-menu");
 
+burgerBtn.addEventListener("click", () => {
+  navMenu.classList.toggle("active");
+});
