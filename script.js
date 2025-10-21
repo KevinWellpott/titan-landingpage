b44c168ba592f64780dc6bc066d59653794114bb
KevinWellpott <rasmus.paweletz@gmail.com>
2025-10-21 16:20:24 +0200

    fertige-version

---
+++ script.js
@@ -0,0 +1,22 @@
+// Konsolen-Check
+console.log("Script geladen ✅");
+
+// --- SIMON ---
+const simonButton = document.getElementById("contact-simon");
+if (simonButton) {
+  simonButton.addEventListener("click", function() {
+    console.log("Simon-Button geklickt ✅");
+    window.open("https://titanagency.de");
+    // oder: window.location.href = "https://titanagency.de";
+  });
+}
+
+// --- KEVIN ---
+const kevinButton = document.getElementById("contact-kevin");
+if (kevinButton) {
+  kevinButton.addEventListener("click", function() {
+    console.log("Kevin-Button geklickt ✅");
+    window.open("https://titanagency.de");
+    // oder: window.location.href = "https://titanagency.de";
+  });
+}
